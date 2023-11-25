9df66026c10de7d53a9409ebd412cd340ec81adc
Kevin Rogers <kevin_t_rogers@hotmail.com>
2023-11-24 22:07:42 -0800

    updated main.js

--- imports/ui/main.js
+++ imports/ui/main.js
@@ -1,9 +1,21 @@
 import { Meteor } from 'meteor/meteor'
 import { createApp } from 'vue'
 import { VueMeteor } from 'vue-meteor-tracker'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 import App from './App.vue'
 import { router } from './router'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+
+  meteor: {
+    clientEntry: 'imports/ui/main.ts',
+  },
+})
 
 Meteor.startup(() => {
   const app = createApp(App)
